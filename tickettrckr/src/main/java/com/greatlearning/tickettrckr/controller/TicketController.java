79888037e6014807e8b838719b075ad3e1a54037
pravin khandare <100687226+pravinKhandare007@users.noreply.github.com>
2023-01-11 09:12:01 +0530

    Add files via upload

---
+++ tickettrckr/src/main/java/com/greatlearning/tickettrckr/controller/TicketController.java
@@ -0,0 +1,37 @@
+package com.greatlearning.tickettrckr.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.greatlearning.tickettrckr.model.Ticket;
+import com.greatlearning.tickettrckr.service.TicketServiceImpl;
+
+@Controller
+public class TicketController {
+	
+	@Autowired
+	TicketServiceImpl ticketService;
+	
+	@GetMapping("tickettrckr/tickets")
+	public String getAlltickets(Model model){
+		List<Ticket> tickets = ticketService.getAllTickets();
+		model.addAttribute("list",tickets);
+		return "htmlfile_1";
+	}
+	
+	@GetMapping("tickettrckr/newTicket")
+	public String newTicket(Model model) {
+		Ticket newTicket = new Ticket();
+		model.addAttribute("newicket",newTicket);
+		return "creation of ticket html file";
+		
+		
+	@PostMapping()
+	}
+	
+}
