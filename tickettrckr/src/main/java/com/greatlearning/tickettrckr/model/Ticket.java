bcd2afb92df6332a525fa87d300dd888687b2b53
pravin khandare <100687226+pravinKhandare007@users.noreply.github.com>
2023-01-18 21:23:40 +0530

    Add files via upload

--- tickettrckr/src/main/java/com/greatlearning/tickettrckr/model/Ticket.java
+++ tickettrckr/src/main/java/com/greatlearning/tickettrckr/model/Ticket.java
@@ -17,7 +17,7 @@ public class Ticket {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ticket_id")
-	private Integer id;
+	private int id;
 	
 	@Column(name = "ticket_title")
 	private String title;
